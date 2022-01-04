e94d29d60be4aa9cc6e233c67df2fa03439a5c45
EugeneKannou <42428147+EugeneKannou@users.noreply.github.com>
2022-01-04 22:52:39 +0500

    Export changes
    I've exported all used functions in case if someone need them :)

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ const getDistance = function (vector1, vector2) {
 }
 
 const dotProduct = function (vector1, vector2) {
+    if (!Array.isArray(vector1) || !Array.isArray(vector2)) {
+        throw new TypeError('Both vectors must be arrays!');
+    }
+
+    if (vector1.length !== vector2.length) {
+        throw ("Arrays must have equal length!");
+    }
+    
     let d = 0.0;
     for (let i = 0; i < vector1.length; i++) {
         d += vector1[i] * vector2[i];
@@ -26,6 +34,10 @@ const dotProduct = function (vector1, vector2) {
 }
 
 const vectorLen = function (vector) {
+    if (!Array.isArray(vector)) {
+        throw new TypeError('Vector must be array!');
+    }
+
     let len = 0.0;
     for (let i = 0; i < vector.length; i++) {
         len += vector[i] ** 2;
@@ -34,4 +46,8 @@ const vectorLen = function (vector) {
     return Math.sqrt(len);
 }
 
-module.exports = getDistance;
+module.exports = {
+    getDistance,
+    vectorLen,
+    dotProduct
+};
