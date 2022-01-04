b22d10d0e1b648d3d0ee971b935495990fd1de13
EugeneKannou <42428147+EugeneKannou@users.noreply.github.com>
2022-01-04 22:49:27 +0500

    Init

---
+++ index.js
@@ -0,0 +1,37 @@
+Number.isInteger = Number.isInteger || function (value) {
+    return typeof value === 'number' &&
+        isFinite(value) &&
+        Math.floor(value) === value;
+};
+
+const getDistance = function (vector1, vector2) {
+    if (!Array.isArray(vector1) || !Array.isArray(vector2)) {
+        throw new TypeError('Both vectors must be arrays!');
+    }
+
+    if (vector1.length !== vector2.length) {
+        throw ("Arrays must have equal length!");
+    }
+
+    return (dotProduct(vector1, vector2) / vectorLen(vector1) / vectorLen(vector1));
+}
+
+const dotProduct = function (vector1, vector2) {
+    let d = 0.0;
+    for (let i = 0; i < vector1.length; i++) {
+        d += vector1[i] * vector2[i];
+    }
+
+    return d;
+}
+
+const vectorLen = function (vector) {
+    let len = 0.0;
+    for (let i = 0; i < vector.length; i++) {
+        len += vector[i] ** 2;
+    }
+
+    return Math.sqrt(len);
+}
+
+module.exports = getDistance;
